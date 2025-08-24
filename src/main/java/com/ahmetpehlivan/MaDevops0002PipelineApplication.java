51ca7f6c866038f1136cdf012a95166daf72d3c4
ahmetpehlivan <ahmetpehlivan@yahoo.com>
2025-08-24 12:42:12 +0300

    init

---
+++ src/main/java/com/ahmetpehlivan/MaDevops0002PipelineApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetpehlivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaDevops0002PipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaDevops0002PipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmetpehlivan/MaDevops0002PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetpehlivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaDevops0002PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
